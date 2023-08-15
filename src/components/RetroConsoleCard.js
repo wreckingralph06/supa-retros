4a4fec1a40c14a617f60b42fd0ab3e25e306e372
wreckingralph06 <ralphwaldocandaza@gmail.com>
2023-08-15 22:40:13 +0800

    Added redirection to update page and initial update form

--- src/components/RetroConsoleCard.js
+++ src/components/RetroConsoleCard.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 const RetroConsoleCard = ({ retroConsole }) => {
   return (
     <div className="retro-console-card">
       <h3>{retroConsole.name}</h3>
       <p>{retroConsole.description}</p>
       <div className="price">{retroConsole.price}</div>
+      <div className="buttons">
+        <Link to={"/" + retroConsole.id}>
+          <i className="material-icons">Edit</i>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,7 +1,65 @@
+import supabase from "../config/supabaseClient";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 const Update = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect(() => {
+    const fetchRetroConsole = async () => {
+      const { data, error } = await supabase
+        .from("retro_consoles")
+        .select()
+        .eq("id", id)
+        .single();
+
+      if (error) {
+        navigate("/", { replace: true });
+      }
+
+      if (data) {
+        setName(data.name);
+        setDescription(data.description);
+        setPrice(data.price);
+        console.log("data: ", data);
+      }
+    };
+
+    fetchRetroConsole();
+  }, [id, navigate]);
+
   return (
     <div className="update-page">
-      <h2>Update</h2>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="description">Description: </label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <label htmlFor="price">Price:</label>
+        <input
+          type="number"
+          id="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button>Update Retro Console</button>
+        {/* {formError && <p className="error">{formError}</p>} */}
+      </form>
     </div>
   );
 };
